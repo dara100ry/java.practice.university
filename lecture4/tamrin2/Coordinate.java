ca53319d07c626c42de1e3266fa09c16034206b7
dara100ry <96252204+dara100ry@users.noreply.github.com>
2022-04-02 21:58:16 +0430

    Create  Coordinate.java

---
+++ lecture4/tamrin2/Coordinate.java
@@ -0,0 +1,11 @@
+package tamrin2;
+public class Coordinate<first,second,third> implements Cloneable {
+    public first name;
+    public second age;
+    public third Id;
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+}
