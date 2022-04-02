66e7134abe4f7d590607061eaa14b25f66d7187b
dara100ry <96252204+dara100ry@users.noreply.github.com>
2022-04-02 22:08:15 +0430

    Create mainClass.java

---
+++ lecture4/tamrinClassi/mainClass.java
@@ -0,0 +1,20 @@
+package tamrinClassi;
+
+public class mainClass {
+    public static void main(String[] args) {
+        info obj1 =new info();
+        obj1.setName("dara");
+        obj1.setFamily("sadri");
+        obj1.setAge(18);
+        obj1.setPass(1234);
+        obj1.setUsername("dara100ry");
+        obj1.setJob("programmer");
+        System.out.println(obj1.getName());
+        System.out.println(obj1.getFamily());
+        System.out.println(obj1.getAge());
+        System.out.println(obj1.getPass());
+        System.out.println(obj1.getUsername());
+        System.out.println(obj1.getJob());
+
+    }
+}
